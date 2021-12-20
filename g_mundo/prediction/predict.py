837b914b744ba7441e8b089ac41f7e6f4635f25d
Grigoriy Sterin <gr.sterin@gmail.com>
2021-12-19 21:33:18 -0500

    Prediction module update  implemented get neighbors split method + some refactoring

--- g_mundo/prediction/predict.py
+++ g_mundo/prediction/predict.py
@@ -1,67 +1,89 @@
-def get_num_of_voting_neighbors(node, dsd_matrix, z: int) -> Tuple[int, int]:
-"""
-This method would allow us to use a single parameter for a number of neighbors instead of 2 parameter, which would simplify grid search.
-It also allows to independently determine number of neighbors to use in knn for each node.  
+from typing import Tuple, Dict, List, Callable
 
-Parameters:
-    node - node in target network for which we want to determine num of neighbors to use in knn voting
-dsd_matrix - dsd matrix of target network
-    z - cumulative number of DSD and MUNK neighbors to use in knn
 
-Returns:
-    Tuple[num_of_dsd_neighbors, num_of_munk_neighbors], where num_of_dsd_neighbors + num_of_munk_neighbors = z
-"""
+def get_neighbors_split(protein: str,
+                        target_map: Dict[str, List[str]],
+                        n_neighbors: int,
+                        target_go_f: Callable[[str], List[str]]) -> Tuple[int, int]:
+    """
+    This method would allow us to use a single parameter for a number of neighbors instead of 2 parameter, which would simplify grid search.
+    It also allows to independently determine number of neighbors to use in knn for each node.
+    Parameters:
+        protein         - protein in target network for which we want to determine num of neighbors to use in knn voting
+        target_map      - Dict[protein(target), [protein(target)]] which is sorted protein neighbors by their similarity for the target species (lower index in the list means the protein is more similar)
+        n_neighbors     - No of neighbors for source plus target.
+        target_go_f     - function that takes target protein and returns the GO labels associated with it
+    Returns:
+        Tuple[num_of_dsd_neighbors, num_of_munk_neighbors], where num_of_dsd_neighbors + num_of_munk_neighbors = n_neighbors
+    """
 
-    z_dsd_neighbors = get_z_closest_neighbors(node, dsd_matrix)
-    num_annotated_in_z_neighborhood = len([neighbor for neighbor in z_dsd_neighbors if is_node_annotated(neighbor)])
-    t = num_annotated_in_z_neighborhood / z
+    def get_n_closest_neighbors() -> List[str]:
+        neighbors = target_map[protein] if protein in target_map else []
+        return neighbors[:n_neighbors]
 
-    num_of_dsd_neighbors = int(t * z)
-    num_of_munk_neighbors = int((1 - t) * z)
-    return num_of_dsd_neighbors,  num_of_munk_neighbors
+    def is_protein_annotated(prot: str) -> bool:
+        go_annotations = target_go_f(prot)
+        return len(go_annotations) != 0
 
-def get_weight_coefficient(networks_hubalign_score: float, networks_reference_score1: float, networks_reference_score2: float, alpha1: float, alpha2: float):
-"""
-Let's assume that there's a linear dependency between network similarity score and best weight factor alpha.
-In this case if we empirically determine good weight factors for some 2 network pairs, we'll be able to compute weight factor for any pair of networks knowing their similarity score.
+    target_closest_neighbors = get_n_closest_neighbors()
+    num_closest_neighbors_annotated = len([neighbor for neighbor in target_closest_neighbors if is_protein_annotated(neighbor)])
+    t = num_closest_neighbors_annotated / n_neighbors
 
-Parameters:
-    networks_hubalign_score - mean HubAlign score of 300 highest scored node pairs of a pair of networks we want to find weights for
-    networks_reference_score1 - mean HubAlign score of 300 highest scored node pairs of a pair of networks (e.g., highly similar Human and Mouse)
-    networks_reference_score2 - mean HubAlign score of 300 highest scored node pairs of a different pair of networks (e.g., less similar Human and Baker's yeast)
-    alpha1 - empirically determined best weight factor for the pair of networks used in networks_reference_score1
-    alpha2 - empirically determined best weight factor for the pair of networks used in networks_reference_score2
-Returns:
-    alpha: weight factor for the target network DSD votes for the case of interest.
-"""
+    num_of_dsd_neighbors = int(t * n_neighbors)
+    num_of_munk_neighbors = int((1 - t) * n_neighbors)
 
-# using notation y = kx + b
+    return num_of_dsd_neighbors, num_of_munk_neighbors
+
+
+def get_weight_coefficient(networks_hubalign_score: float,
+                           networks_reference_score1: float,
+                           networks_reference_score2: float,
+                           alpha1: float,
+                           alpha2: float):
+    """
+    Let's assume that there's a linear dependency between network similarity score and best weight factor alpha.
+    In this case if we empirically determine good weight factors for some 2 network pairs, we'll be able to compute weight factor for any pair of networks knowing their similarity score.
+
+    Parameters:
+        networks_hubalign_score - mean HubAlign score of 300 highest scored node pairs of a pair of networks we want to find weights for
+        networks_reference_score1 - mean HubAlign score of 300 highest scored node pairs of a pair of networks (e.g., highly similar Human and Mouse)
+        networks_reference_score2 - mean HubAlign score of 300 highest scored node pairs of a different pair of networks (e.g., less similar Human and Baker's yeast)
+        alpha1 - empirically determined best weight factor for the pair of networks used in networks_reference_score1
+        alpha2 - empirically determined best weight factor for the pair of networks used in networks_reference_score2
+    Returns:
+        alpha: weight factor for the target network DSD votes for the case of interest.
+    """
+
+    # using notation y = kx + b
     k = (alpha2 - alpha1) / (networks_reference_score2 - networks_reference_score1)
     b = alpha1 - networks_reference_score1 * k	
     alpha = k * networks_hubalign_score + b
     
     return alpha
 
-def MUNDO_predict(target_map, 
-                  MUNK_map, 
-                  n_neighbors,
-                  target_go_f,
-                  source_go_f,
-                  MUNK_weight = 0.25):
+
+def MUNDO_predict(target_map: Dict[str, List[str]],
+                  MUNK_map: Dict[str, List[str]],
+                  n_neighbors: int,
+                  target_go_f: Callable[[str], List[str]],
+                  source_go_f: Callable[[str], List[str]],
+                  MUNK_weight: float = 0.25) -> Dict[str, List[Tuple[str, float]]]:
     """
-    Performs prediction on the target network, 
-    target_map: map protein(target) -> [protein(target)] which is sorted protein neighbors by their similarity for the target species(lower index in the list means the protein is more similar)
-    
-    MUNK map: map protein(target) -> [protein (source)] where the protein in this case is from the source species.
-    
-    n_neighbors                   -> integer values. No of neighbors for
-    source plus target. [TODO]: needs to be splitted properly by Grigorii's algorithm.
-    
-    target_go_f, source_go_f : both functions that takes target (or source if the function is MUNK_go_f) and returns the GO labels associated with them.
+    Performs prediction on the target network,
+    Parameters:
+        target_map      - Dict[protein(target), [protein(target)]] which is sorted protein neighbors by their similarity for the target species(lower index in the list means the protein is more similar)
+        MUNK_map        - Dict[protein(target), [protein(source)]] where the protein in this case is from the source species.
+        n_neighbors     - integer values. No of neighbors for source plus target.
+        target_go_f     - both functions that takes target protein and returns the GO labels associated with it.
+        source_go_f     - both functions that takes source protein and returns the GO labels associated with it.
+        MUNK_weight     - weight to use for MUNK votes.
+    Returns:
+        Dictionary containing mapping from target proteins to the sorted (in descending order) list of associated labels and their confidence values
     """
-    def vote(target_voters, MUNK_voters):
+
+    def vote(target_voters: List[str], MUNK_voters: List[str]) -> List[Tuple[str, float]]:
         """
-        Returns a map GO -> confidence value.
+        Returns a list of (GO, confidence value) tuples
         """
         go_map = {}
         
@@ -69,31 +91,23 @@ def MUNDO_predict(target_map,
         for v in target_voters:
             go_labels = target_go_f(v)
             for g in go_labels:
-                go_map[g] = 1 if g not in go_map else go_map[g] + 1
+                go_map[g] = 1.0 if g not in go_map else go_map[g] + 1.0
         
         # Work on source
         for v in MUNK_voters:
             go_labels = source_go_f(v)
             for g in go_labels:
                 go_map[g] = MUNK_weight if g not in go_map else go_map[g] + MUNK_weight
-        
-        return sorted(go_map.items(), reverse = True, lambda k: k[1]) # [(go_label, vote), ... ] format, vote is float
-    
-    def get_neigbors_split(protein):
-      """
-      [TODO:] Grigorii Fill it out.
-      """
-      # How to split the 
-      return n_target_neighbors, n_MUNK_neighbors
-    
-    # 
-    n_target_neighbors, n_MUNK_neighbors = get_neighbors_split(protein)
+
+        return sorted(go_map.items(), reverse=True, key=lambda k: k[1])  # [(go_label, vote), ... ] format, vote is float
     
     # Get all the target proteins
-    proteins       = target_map.keys()
+    proteins = target_map.keys()
     protein_labels = {}
     
     for p in proteins:
+        n_target_neighbors, n_MUNK_neighbors = get_neighbors_split(p, target_map, n_neighbors, target_go_f)
+
         label = target_go_f(p)
         
         # Only happens if training proteins, skip them 
@@ -105,4 +119,5 @@ def MUNDO_predict(target_map,
         MUNK_voters   = MUNK_map[p][:n_MUNK_neighbors]
         
         protein_labels[p] = vote(target_voters, MUNK_voters)
-    return protein_labels[p]
+
+    return protein_labels
