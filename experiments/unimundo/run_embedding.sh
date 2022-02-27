be8a9cedd476c1e80cc30eb41a19d40d4f34d78b
kap-devkota <kapil.devkota@tufts.edu>
2022-02-27 02:15:50 -0500

    Added setup changes

--- experiments/unimundo/run_embedding.sh
+++ experiments/unimundo/run_embedding.sh
@@ -22,6 +22,6 @@ while getopts "s:d:m:p:" args; do
 done
 
 WORKING_FOLDER=/cluster/tufts/cowenlab/Projects/UNIMUNDO/MUNDO/training_dir
-OUTPUT=$WORKING_FOLDER/network_logs/${NETWORK}_${DIM}_N2VEC.log
+OUTPUT=$WORKING_FOLDER/network_logs/${NETWORK}_${DIM}_UMUNDO.log
 
 sbatch $SBATCH_OPTS -o ${OUTPUT} ./unimundo_embed.py --working_folder=${WORKING_FOLDER} --biogrid_tsv_folder=${WORKING_FOLDER} --source_organism_name=${SOURCE} --target_organism_name=${TARGET} --mapping=$MAPPING} --mapping_num_of_pairs=${DIM} --construct_dsd --construct_dsd_dist --construct_kernel --save_munk_matrix --verbose
--- gmundo/alignment.py
+++ gmundo/alignment.py
@@ -12,24 +12,16 @@ def isorank(G1, G2, row_map, col_map, alpha, matches, E = None, iterations = 5):
     
     E : numpy matrix {m x n}, sequence similarity score
     """
-    def _isorank_compute_A():
-        """
-        Function to compute the isorank score for the networks G1 and G2.
-        G1: networkx graph
-        G2: networkx graph
-        A : A[i, j][u, v]
-        """
-        A    = np.zeros((len(row_map), len(col_map), len(row_map), len(col_map)))
-        for i, i_id in row_map.items():
-            for j, j_id in col_map.items():
-                for u, u_id in row_map.items():
-                    for v, v_id in col_map.items():
-                        A[i, j, u, v] = (1. / (len(G1[r]) * len(G2[c])) if 
-                                        (G1.has_edge(i, u) and G2.has_edge(j, v)) 
-                                         else 0)
+    def _isorank_compute_Aij(i, j, m, n):
+        A = np.zeros((m, n))             
+        for u, u_id in row_map.items():
+            for v, v_id in col_map.items():
+                A[u_id, v_id] = (1. / (len(G1[u]) * len(G2[v])) if 
+                          (G1.has_edge(i, u) and G2.has_edge(j, v)) 
+                          else 0)
         return A
     
-    def _isorank_compute_next_r(A, R, E = None, alpha = 1):
+    def _isorank_compute_next_r(R, E = None, alpha = 1):
         """
         Performs the R = AR operation, where R is a matrix and A is a 4 dimensional tensor.
         """
@@ -37,7 +29,7 @@ def isorank(G1, G2, row_map, col_map, alpha, matches, E = None, iterations = 5):
         R_next = np.zeros((m, n))
         for i in range(m):
             for j in range(n):
-                R_next[i, j] = alpha * np.sum(A[i, j] * R)
+                R_next[i, j] = alpha * np.sum(_isorank_compute_Aij(i, j, m, n) * R)
         if E is not None:
             R_next += (1-alpha) * E
         return R_next / norm(R_next)
@@ -61,9 +53,8 @@ def isorank(G1, G2, row_map, col_map, alpha, matches, E = None, iterations = 5):
     errors = []
     
     # Compute Isorank matrix
-    A      = _isorank_compute_A()
     for i in range(iterations):
-        R_next = _isorank_compute_next_r(A, R, E, alpha)
+        R_next = _isorank_compute_next_r(R, E, alpha)
         errors.append(np.linalg.norm(R - R_next, ord = "fro"))
         R      = R_next
     
