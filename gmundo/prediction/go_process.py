d8a18b98e7c7a03dfaea2bafa1728d6d8d3b211e
kap-devkota <kapil.devkota@tufts.edu>
2022-03-06 11:37:34 -0500

    added go process

--- gmundo/prediction/go_process.py
+++ gmundo/prediction/go_process.py
@@ -92,15 +92,12 @@ def get_go_labels(filter_protein,
         f_assoc_genes = list(set(assoc_genes).intersection(set(entrez_labels))) 
         
         # Removes the GO terms if it very sparsely annotates the protein list
-        if not ("lower_bound" in filter_protein and len(f_assoc_genes) > filter_protein["lower_bound"]):
-            continue
-
-        # This is used for UNIMUNDO
-        if not("target_gos" in filter_protein and key in filter_protein["target_gos"]):
-            continue
+        lower_bound_satisfied = ("lower_bound" in filter_protein and len(f_assoc_genes) > filter_protein["lower_bound"])
+        target_go_satisfied   = ("target_gos" in filter_protein and key in filter_protein["target_gos"])
         
-        label_dict[key] = f_assoc_genes
-        f_labels.append(key)
+        if lower_bound_satisfied or target_go_satisfied:
+            labels_dict[key] = f_assoc_genes
+            f_labels.append(key)
     log(f"Number of GO-terms: {len(f_labels)}")
     return f_labels, labels_dict
 
