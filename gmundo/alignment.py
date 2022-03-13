42f16647f2573710e3c21c331e830320e41419a7
Grigoriy Sterin <gsterin@tesseratx.com>
2022-03-13 00:05:03 -0500

    Added function for invoking HubAlign

--- gmundo/alignment.py
+++ gmundo/alignment.py
@@ -1,5 +1,8 @@
 import numpy as np
 from numpy.linalg import pinv, norm
+import subprocess
+import pathlib
+import os
 
 ################### ISORANK CODE #############################
 def isorank(G1, G2, row_map, col_map, alpha, matches, E = None, iterations = 5):
@@ -67,4 +70,54 @@ def isorank(G1, G2, row_map, col_map, alpha, matches, E = None, iterations = 5):
     best_pairs = [(i_row_map[p], i_col_map[q]) for p, q in best_pairs]
     
     return best_pairs, R, errors
-                      
+
+
+def hubalign(smaller_network_file_name: str,
+             bigger_network_file_name: str,
+             input_folder: str,
+             output_folder: str,
+             lmbda: float = 0.1,
+             alpha: float = 1,
+             blast_file: str = None) -> str:
+    """
+    Parameters:
+        smaller_network_name - name of the network file with a smaller number of nodes
+        bigger_network_name - name of the network file with a bigger number of nodes
+        input_folder - location of the input network files
+        output_folder - location for the output alignment file
+        lmbda - parameter which controls importance of the edge weight compared to node weight
+        alpha - parameter which controls importance of sequence similarity compared to topological similarity
+        blast_file - path to a file containing space-separated node pairs and their BLAST similarity scores
+    Returns:
+        path to the alignment file
+    """
+    hubalign_binary_path = f"{pathlib.Path(__file__).parent.absolute()}/bin/hubalign-with-scores"
+    hubalign_call_array = [hubalign_binary_path,
+                           smaller_network_file_name,
+                           bigger_network_file_name,
+                           "-i", input_folder,
+                           "-o", output_folder,
+                           "-l", str(lmbda),
+                           "-a", str(alpha)]
+
+    if alpha != 1 and blast_file is not None:
+        hubalign_call_array.extend(["-b", blast_file])
+    elif alpha == 1 and blast_file is not None:
+        raise Exception("HubAlign error: if alpha is equal to 1, blast file should not be passed"
+                        "into function")
+    elif alpha != 1 and blast_file is None:
+        raise Exception("HubAlign error: if blast file is passed into function, alpha should not be 1")
+
+    process = subprocess.Popen(hubalign_call_array,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    if process.returncode != 0:
+        raise Exception(f"HubAlign error: process executed with errors. Return code is {process.returncode}. "
+                        f"Error: {err.decode('UTF-8')}")
+
+    alignment_file_path = f"{output_folder}/{smaller_network_file_name}-{bigger_network_file_name}.alignment"
+    if not os.path.exists(alignment_file_path):
+        raise Exception(f"HubAlign error: process executed with errors: alignment file doesn't "
+                        f"exist in {output_folder}")
+    return alignment_file_path
