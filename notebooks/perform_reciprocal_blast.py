9e468e00d8b0d2635da873f393dd8cc815fa8ded
kap-devkota <kapil.devkota@tufts.edu>
2022-07-07 16:00:13 -0400

    add new blast binaries

--- notebooks/perform_reciprocal_blast.py
+++ notebooks/perform_reciprocal_blast.py
@@ -1,7 +1,8 @@
 #!/cluster/tufts/cowenlab/.envs/denoise/bin/python
-import subprocess
-import multiprocessing as mp
-from multiprocessing import Pool
+
+# import subprocess
+# import multiprocessing as mp
+# from multiprocessing import Pool
 import sys
 import os
 import time
@@ -20,15 +21,6 @@ class BlastOP():
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--blast_loc", 
-                        help = "location of the blastp binary. If the blastp program is accessible everywhere, just use `blastp`", 
-                        default = "blastp")
-    parser.add_argument("--no_threads_for_blast", type = int, default = 5, 
-                       help = "The number of threads allocated for blast")
-    parser.add_argument("--source_fasta", help = "location of the source fasta file", 
-                        required = True)
-    parser.add_argument("--target_fasta", help = "location of the target fasta file",
-                       required = True)
     parser.add_argument("--out_forward", help = "forward output location",
                        required = True)
     parser.add_argument("--out_reverse", help = "backward output location",
@@ -37,23 +29,6 @@ def get_args():
                        required = True)
     parser.add_argument("--verbose", default = False, action = "store_true")
     return parser.parse_args()
-
-def run_blast_command(blast_loc, 
-                      n_threads,
-                      query,
-                      subject,
-                      out_loc,
-                      out_fmt):
-    """
-        RUNNING THE blastp command
-    """
-    command = [blast_loc, "-out", out_loc, "-outfmt", str(out_fmt), "-query", query, "-subject", subject,  "-num_threads", str(n_threads)]
-    try:
-        proc    = subprocess.run(command, check = True, capture_output = True)
-    except Exception as e:
-        raise Exception(f"Error: {str(e)}")
-    return "\t OP: --- " + proc.stdout.decode("utf-8") + "\n Errors: ---" + proc.stderr.decode("utf-8")
-
 
     
 def main(args):
@@ -64,71 +39,6 @@ def main(args):
     def log(strng):
         if args.verbose:
             print(strng)
-    
-    pool = Pool()
-    
-    rp1, rp2 = False, False
-    r1       = BlastOP()
-    r2       = BlastOP()
-    if not os.path.exists(args.out_forward):
-        rp1 = True
-        p1 = pool.apply_async(run_blast_command, 
-                               args = (args.blast_loc,
-                                args.no_threads_for_blast,
-                                args.source_fasta,
-                                args.target_fasta,
-                                args.out_forward,
-                                      6), callback = r1.update)
-    if not os.path.exists(args.out_reverse):
-        rp2 = True
-        p2 = pool.apply_async(run_blast_command, 
-                              (args.blast_loc,
-                               args.no_threads_for_blast,
-                               args.target_fasta,
-                               args.source_fasta,
-                               args.out_reverse,
-                              6), callback = r2.update)
-   
-    procs = []
-    try:
-        procs.append(p1)
-    except NameError:
-        procs = []
-    
-    try:
-        procs.append(p2)
-    except NameError:
-        pass
-    
-    print("Waiting for Reciprocal Blast to complete...")
-    print(f"Running BLAST procs: {len(procs)}")
-    # Catch exceptions
-    while True:
-       # Catch exceptions in results not ready
-        try:
-            ready = ((not rp1) or (rp1 and p1.ready())) or ((not rp2) or (rp2 and p2.ready()))
-            successful = ((not rp1) or (rp1 and p1.successful())) or ((not rp2) or (rp2 and p2.successful()))
-            print(successful)
-            print("Here")
-            print(ready)
-        except Exception:
-            continue
-        # If both returned success exit
-        if all(successful):
-            break
-        # If exception occurs report it
-        if all(ready) and not all(successful):
-            raise Exception(f"Exceptions Raised {[str(p._value) for p in procs if not p.successful()]}")
-            break
-    pool.close()
-    pool.join()
-    
-    if rp1:
-        log(f"Output from FORWARD: \n\n {r1.outstring} \n\n" + "*" * 100 + "\n")
-    if rp2:
-        log(f"Output from REVERSE: \n\n {r2.outstring} \n\n" + "*" * 100 + "\n")
-    
-    print("Reciprocal blast completed! loading the blast results...")
     
     fwd = pd.read_csv(args.out_forward, sep = "\t", header = None)
     rev = pd.read_csv(args.out_reverse, sep = "\t", header = None)
@@ -165,7 +75,8 @@ def main(args):
     
     rec = rec.loc[rec["query_x"] == rec["subject_y"]]
     rec = rec.groupby(['query_x', 'subject_x'], as_index = False).max()
-    
+
+    log("Saving output...")
     rec.to_csv(args.final_op, sep = "\t", index = False)
     ####
     
--- notebooks/run_rpblast.sh
+++ notebooks/run_rpblast.sh
@@ -1,8 +1,50 @@
 #!/bin/bash
-SBATCH_OPTS="--mem 64GB --nodes 1 --ntasks 30 --partition preempt --time 1-00:00:00"
+#SBATCH --job-name=rpblast
+#SBATCH --output=rpblast.output
+#SBATCH --time=1-00:00:00
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=5
+#SBATCH --mem-per-cpu=5GB
+#SBATCH --partition=preempt
 
-LOGFILE="op.log.txt"
-sbatch -o $LOGFILE ./perform_reciprocal_blast.py --blast_loc ../binaries/ncbi-blast-2.13.0+/bin/blastp --no_threads_for_blast 5 --source_fasta blast-p-data-in-human-yeast/biogrid_Human_ID_9606_sequences.fasta --target_fasta blast-p-data-in-human-yeast/biogrid_Yeast_ID_559292_sequences.fasta --out_forward forward.log --out_reverse reverse.log --final_op output.tsv --verbose
+BLAST_LOC=../binaries/ncbi-blast-2.13.0+/bin/blastp
+SOURCE_FASTA=
+TARGET_FASTA=
+FORWARD_OUT=
+REVERSE_OUT=
+# final tsv file for reciprocal blast
+FINAL_TSV=
+
+USAGE () {
+    echo 'USAGE:'
+    echo '       ./run_rpblast.sh -s [SOURCE-FASTA-FILE] -t [TARGET-FASTA-FILE] -f [OUTPUT-FILE-LOCATION-FORWARD] -r [OUTPUT-FILE-LOCATION-REVERSE] -o [OUTPUT-TSV-FILE]'
+}  
 
 
+while getopts "s:t:f:r:o:" arg; do
+    case ${arg} in
+	s) SOURCE_FASTA=${OPTARG}
+	   ;;
+	t) TARGET_FASTA=${OPTARG}
+	   ;;
+	f) FORWARD_OUT=${OPTARG}
+	   ;;
+	r) REVERSE_OUT=${OPTARG}
+	   ;;
+	o) FINAL_TSV=${OPTARG}
+    esac
+done
+
+if [ -z $SOURCE_FASTA ] || [ -z $TARGET_FASTA ] || [ -z $FORWARD_OUT ] || [ -z $REVERSE_OUT ] || [ -z $FINAL_TSV ]
+then
+    USAGE
+    exit 1
+fi
+
+# Forward run and backward run and wait
+srun -n 1 $BLAST_LOC -out $FORWARD_OUT -outfmt 6 -query $SOURCE_FASTA -subject $TARGET_FASTA -num_threads 5 &
+srun -n 1 $BLAST_LOC -out $REVERSE_OUT -outfmt 6 -query $TARGET_FASTA -subject $SOURCE_FASTA -num_threads 5 &
+wait
+
+srun -n 1 python perform_reciprocal_blast.py --out_forward $FORWARD_OUT --out_reverse $REVERSE_OUT --final_op $FINAL_TSV --verbose
 
